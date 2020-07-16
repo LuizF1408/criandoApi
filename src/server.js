2c036fa3f1ad43283cd81f3e910364a60e925091
Luiz F <liko.12@hotmail.com>
2020-07-15 22:20:38 -0300

    3 formas de token

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 const express = require ('express');
-const db = require('./dbconection');
+require('./dbconection');
 const Restaurant = require('./restaurant');
-
+const User = require('./userModel');
+const uuid = require('uuid')
 
 
 const app = express ();
 app.use(express.json())
 //Pesquisa todos restaurantes funcionando
 app.get('/restaurants', async(req,res)=>{
+    // token 1
+    // const token = req.query.token
+    // if (token != 'lasanha'){
+    //     res.status(401).json({message: 'Acesso negado: token inválido'});
+    //     return;
+    // }
+
+    //token 2
+    // const token = req.header('token')
+    // if (token != 'lasanha'){
+    //     res.status(401).json({message: 'Acesso negado: token inválido'});
+    //     return;
+    // }
+
     const restaurants = await Restaurant.find();
     res
     .status(200)
@@ -16,21 +31,23 @@ app.get('/restaurants', async(req,res)=>{
 
 })
 //Pesquisa por Bairro funcionando
-app.get('/restaurants/:borough',async(req,res) =>{
+app.get('/restaurants/borough/:borough',async(req,res) =>{
+   
     const borough = req.params.borough;
   var restaurant = await Restaurant.find({borough:borough})
-  if(!restaurant){
-      res.status(404).json({message: 'Restaurante não encostrado'})
+  if(restaurant.length < 1){
+      res.status(404).json({message: 'Bairro não encostrado'})
+      console.log('Rota do bairro')
   }
   res.status(200).json(restaurant)
   
 
 })
 //Pesquisa por nome funcionando
-app.get('/restaurants/:name',async(req,res) =>{
+app.get('/restaurants/name/:name',async(req,res) =>{
     const name = req.params.name;
   var restaurant = await Restaurant.find({name:name})
-  if(!restaurant){
+  if(restaurant.length < 1){
       res.status(404).json({message: 'Restaurante não encostrado'})
   }
   res.status(200).json(restaurant)
@@ -71,6 +88,31 @@ app.delete('/restaurants/:nome', async(request, response) => {
 
 
 // })
+
+app.post('/signup',async(req,res)=> {
+    const email = req.body.email;
+    const user = await User.findOne({email:email});
+    console.log(user)
+    if (user){
+        res.status(200).json({message:'Usuário já existente :',token: user.token})
+        return;
+    }
+    const token = uuid.v4()
+    const newUser = new User({
+        email : email,
+        token : token,
+
+    });
+
+    newUser.save()
+    .then(()=>{
+        res.status(201).json({message:'Token gerado :',token: token})
+    })
+    .catch((error)=>{
+        res.status(400).json({message: error.message});
+
+    })
+})
 
 app.listen(3000,()=> {
     console.log('Server online')
